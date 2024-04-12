c78533df8b226744bc2c37e677c164c2954dce3b
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-04-12 10:53:35 +0200

    Add project files.

---
+++ Counter/Counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Counter
+    {
+
+        int value = 0;
+        public void Increment()
+        {
+            value++;
+        }
+
+       
+        public void Reset()
+        {
+            value = 0;
+        }
+
+      
+        public int GetValue()
+        {
+            
+            return value;
+        }
+    }
+
+
+}
